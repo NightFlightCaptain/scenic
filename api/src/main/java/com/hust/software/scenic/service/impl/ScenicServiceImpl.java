22787528e9e62b4b8d8fb7472f83f7ed4ff58ed9
1260556188 <318325310@qq.com>
2019-10-27 14:06:06 +0800

    Update：ScenicServiceImpl

--- api/src/main/java/com/hust/software/scenic/service/impl/ScenicServiceImpl.java
+++ api/src/main/java/com/hust/software/scenic/service/impl/ScenicServiceImpl.java
@@ -1,12 +1,20 @@
 package com.hust.software.scenic.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hust.software.scenic.common.CommonResult;
+import com.hust.software.scenic.mgb.mapper.PictureMapper;
+import com.hust.software.scenic.mgb.mapper.PictureScenicMapper;
 import com.hust.software.scenic.mgb.mapper.ScenicMapper;
+import com.hust.software.scenic.mgb.model.Picture;
+import com.hust.software.scenic.mgb.model.PictureScenic;
+import com.hust.software.scenic.mgb.model.PictureScenicExample;
 import com.hust.software.scenic.mgb.model.Scenic;
 import com.hust.software.scenic.mgb.model.ScenicExample;
 import com.hust.software.scenic.service.ScenicService;
@@ -17,6 +25,12 @@ public class ScenicServiceImpl implements ScenicService {
 	@Autowired
 	private ScenicMapper scenicMapper;
 	
+	@Autowired
+	private PictureScenicMapper pictureScenicMapper;
+	
+	
+	@Autowired
+	private PictureMapper pictureMapper;
 	@Override
 	public CommonResult addScenic(Scenic scenic) {
 		boolean flag = scenicMapper.insert(scenic)>=1;
@@ -47,14 +61,32 @@ public class ScenicServiceImpl implements ScenicService {
 		}
 	}
 
+	/**
+	 * 查询单个景点的信息，并将景点的图片找到后一起封装返回
+	 */
 	@Override
 	public CommonResult selScenicById(Integer scenicId) {
 		Scenic scenic = scenicMapper.selectByPrimaryKey(scenicId);
+		
 		if(scenic==null) {
 			return CommonResult.failed("该景点信息查询失败");
-		}else {
-			return CommonResult.success("该景点信息查询成功", scenic);
 		}
+		
+		List<Picture> list = new ArrayList<Picture>();
+		Map<String,Object> map = new HashMap<>();
+		PictureScenicExample pse = new PictureScenicExample();
+		PictureScenicExample.Criteria criteria = pse.createCriteria();
+		criteria.andScenicIdEqualTo(scenicId);
+		List<PictureScenic> pictureScenicList = pictureScenicMapper.selectByExample(pse);
+		
+		for (PictureScenic p:pictureScenicList) {
+			Picture picture = pictureMapper.selectByPrimaryKey(p.getPictureId());
+			list.add(picture);
+		}
+		map.put("scenic", scenic);
+		map.put("scenicpicture", list);
+
+		return CommonResult.success("该景点信息查询成功", map);
 	}
 
 	@Override
